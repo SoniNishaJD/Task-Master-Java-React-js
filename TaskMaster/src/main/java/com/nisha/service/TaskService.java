336f79b82c0918b4bac63663740a86d6d66fa7a3
Soni Nisha <soni.nishaa24@gmail.com>
2024-11-14 14:11:41 +0530

    first commit

---
+++ TaskMaster/src/main/java/com/nisha/service/TaskService.java
@@ -0,0 +1,27 @@
+package com.nisha.service;
+
+import com.nisha.model.Task;
+import com.nisha.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+}
